21ce982ff3c094832c441d0c9ee3ad12428966a9
fabriariex-22 <fabricio.crespo.requena@gmail.com>
2020-11-16 17:38:00 -0400

    Creando el main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { SignupComponent } from './components/user/signup/signup.component';
+import { MainComponent } from './menu/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SignupComponent
+    SignupComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
