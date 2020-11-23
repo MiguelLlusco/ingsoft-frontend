aeb8c161f6f88aacb2c0c2591aa07815be288a22
typhoon-j <gabriel.cordon.santacruz@gmail.com>
2020-11-22 21:31:44 -0400

    Conexion con backend y eliminacion de folder store

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: any[] = [];
+
+  constructor(private http: HttpClient) { 
+
+    this.http.get('http://localhost:8080/users/1')
+      .subscribe((data: any) => {
+        this.user = data;
+        console.log(this.user);
+        }
+      )
+      
+      
+   }
 
   ngOnInit(): void {
   }
